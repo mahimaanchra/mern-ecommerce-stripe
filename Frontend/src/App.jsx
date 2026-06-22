862f5ebaf5227dcc6d8466525089b389d41be62d
mahimaanchra <mahimaanchra@gmail.com>
2026-06-22 08:54:19 -0700

    routing done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Routes } from 'react-router-dom'
+import {Routes , Route} from 'react-router-dom'
+import PlaceOrder from './pages/PlaceOrder'
 const App = () => {
   return (
-    <div>
-      
+    <div className= 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+     <Routes>
+      <Route path = '/' element={<Home />} />
+      <Route path = '/collection' element={<Collection />} />
+      <Route path = '/about' element={<About />} />
+      <Route path = '/contact' element={<Contact />} />
+      <Route path = '/product/:productId' element={<ProductDetails />} />
+      <Route path = '/cart' element={<Cart />} />
+      <Route path = '/login' element={<Login />} />
+      <Route path = '/placeorder' element={<PlaceOrder/>}/>
+      <Route path = '/orders' element={<Orders/>}/>
+      </Routes> 
     </div>
   )
 }
