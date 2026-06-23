db96b457f794138781bc930665add472b2fb5c19
mahimaanchra <mahimaanchra@gmail.com>
2026-06-23 10:40:32 -0700

    added navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
 import {Routes , Route} from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
+import Home from '../src/pages/Home'
+import Collection from '../src/pages/Collection'
+import About from '../src/pages/About'
+import Contact from '../src/pages/Contact'
+import Product from '../src/pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
 const App = () => {
   return (
     <div className= 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
      <Routes>
       <Route path = '/' element={<Home />} />
       <Route path = '/collection' element={<Collection />} />
       <Route path = '/about' element={<About />} />
       <Route path = '/contact' element={<Contact />} />
-      <Route path = '/product/:productId' element={<ProductDetails />} />
+      <Route path = '/product/:productId' element={<Product />} />
       <Route path = '/cart' element={<Cart />} />
       <Route path = '/login' element={<Login />} />
       <Route path = '/placeorder' element={<PlaceOrder/>}/>
