9563ff1e72d138dea52fe8e5f1386b0033ebf218
mahimaanchra <mahimaanchra@gmail.com>
2026-06-25 04:36:29 -0700

    completed ShopContextProvider

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,3 +1,18 @@
 import { createContext } from "react";
+import {products} from "../assets/frontend_assets/assets"
+export const ShopContext = createContext();
 
-const ShopContext = createContext();
+const ShopContextProvider = (props) =>{
+    const currency = '$';
+    const delivery_fee = 10;
+    const value = {
+     products , currency , delivery_fee
+    }
+    return (
+        <ShopContextProvider value={value}>
+            {props.children}
+        </ShopContextProvider>
+    )
+}
+
+export default ShopContextProvider;
