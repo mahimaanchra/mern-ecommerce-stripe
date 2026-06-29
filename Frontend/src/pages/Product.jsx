7c1b579e9992f77f0c5d5383bae2ab905f3d737f
mahimaanchra <mahimaanchra@gmail.com>
2026-06-29 07:07:21 -0700

    product display completed

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/frontend_assets/assets';
 
 const Product = () => {
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products , currency} = useContext(ShopContext);
   const [productData , setProductData] = useState(false);
   const [image , setImage] = useState('');
+  const [size , setSize] = useState('');
 
   const fetchProductData = () => {
      products.map((item)=>{
@@ -31,12 +33,56 @@ const Product = () => {
       <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
         {
           productData.image.map((item,index)=>(
-            <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
+            <img onClick={()=>setImage(item)}  src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
           ))
         }
       </div>
+      <div className='w-full sm:w-[80%]'>
+        <img src={image } className='w-full h-auto' alt=''/>
       </div>
       </div>
+      {/*Product Info*/}
+      <div className=' flex-1 '>
+        <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+        <div className='flex items-center gap-1 mt-2'>
+         <img src={assets.star_icon} alt="" className="w-3 5" />
+         <img src={assets.star_icon} alt="" className="w-3 5" />
+         <img src={assets.star_icon} alt="" className="w-3 5" />
+         <img src={assets.star_icon} alt="" className="w-3 5" />
+         <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+         <p className='pl-2'>(122)</p>
+        </div>
+        <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+        <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+        <div className='flex flex-col gap-4 my-8'>
+          <p>Select Size</p>
+          <div className='flex gap-2'>
+            {productData.sizes.map((item,index)=>(
+              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+            ))}
+          </div>
+        </div>
+        <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+        <hr  className='mt-8 sm:w-4/5'/>
+        <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+          <p>100% Original product.</p>
+          <p>Cash on delivery is vailable on this product.</p>
+          <p>Easy return and exchange policy within 7 days.</p>
+        </div>
+      </div>
+      </div>
+      {/*Description andn Reveiw Section*/}
+      <div className='mt-20'>
+       <div className='flex'>
+        <b className='border px-5 py-3 text-sm'>Description</b>
+        <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+       </div>
+       <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quidem maiores illo quis facilis aperiam repudiandae in, nisi possimus, laborum a asperiores hic impedit laboriosam! Eaque neque quo dicta alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, dolor deserunt ducimus porro quisquam soluta tempora illum quis voluptate laboriosam veritatis quam rerum nesciunt voluptatem odit? Officia sequi expedita excepturi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quidem maiores illo quis facilis aperiam repudiandae in, nisi possimus, laborum a asperiores hic impedit laboriosam! Eaque neque quo dicta alias.</p>
+       </div>
+      </div>
+      {/*Display Related Products*/}
     </div>
   ) : <div className='opacity=0'></div>
 }
