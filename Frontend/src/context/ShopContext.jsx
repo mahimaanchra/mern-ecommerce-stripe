3d083d09086e670afeb70f03d4d4f40531d0a30a
mahimaanchra <mahimaanchra@gmail.com>
2026-06-30 09:00:00 -0700

    product page completed

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import {products} from "../assets/frontend_assets/assets"
 export const ShopContext = createContext();
 
@@ -7,8 +7,30 @@ const ShopContextProvider = (props) =>{
     const delivery_fee = 10;
     const [search , setSearch] = useState('');
     const [showSearch , setShowSearch] = useState(false);
+    const [cartItems , setCartItems] = useState({});
+
+    const addToCart = async (itemId , size) => {
+     let cartData = structuredClone(cartItems);
+
+     if(cartData[itemId]){
+        if(cartData[itemId][size]){
+            cartData[itemId][size] += 1;
+        }else{
+            cartData[itemId][size] = 1;
+        }
+     }else{
+        cartData[itemId] = {};
+        cartData[itemId][size] = 1;
+     }
+     setCartItems(cartData);
+    }
+
+    useEffect(()=>{
+
+    },[cartItems])
+
     const value = {
-     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch
+     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart 
     }
     return (
         <ShopContext.Provider value={value}>
