f6ee24c20165c51eaa401b1b1206b7cfa06103c8
mahimaanchra <mahimaanchra@gmail.com>
2026-07-01 11:43:20 -0700

    updating no. of cartItems

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -32,12 +32,24 @@ const addToCart = async (itemId , size) => {
      setCartItems(cartData);
     }
 
-    useEffect(()=>{
-
-    },[cartItems])
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                  if(cartItems[items][item]>0){
+                    totalCount += cartItems[items][item];
+                  }  
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
 
     const value = {
-     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart 
+     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart , getCartCount
     }
     return (
         <ShopContext.Provider value={value}>
