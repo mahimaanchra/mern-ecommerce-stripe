8649f70c65095df5b449b09de845595c8e6df556
mahimaanchra <mahimaanchra@gmail.com>
2026-07-03 23:30:00 -0700

    PlaceOrder

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import {products} from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) =>{
@@ -9,6 +11,8 @@ const ShopContextProvider = (props) =>{
  const [search , setSearch] = useState('');
  const [showSearch , setShowSearch] = useState(false);
  const [cartItems , setCartItems] = useState({});
+
+ const navigate = useNavigate();
  
 const addToCart = async (itemId , size) => {
 
@@ -72,7 +76,7 @@ const addToCart = async (itemId , size) => {
     }
 
     const value = {
-     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart , getCartCount , updateQuantity , getCartAmount
+     products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart , getCartCount , updateQuantity , getCartAmount , navigate
     }
     return (
         <ShopContext.Provider value={value}>
--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -3,26 +3,27 @@ import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
 import { assets } from '../assets/frontend_assets/assets';
 import CartTotal from '../components/CartTotal';
-const Cart = () => {
-  const {products , currency , cartItems , updateQuantity } = useContext(ShopContext);
 
+const Cart = () => {
+  const { products , currency , cartItems , updateQuantity , navigate } = useContext(ShopContext);
   const [cartData , setCartData] = useState([]);
 
-  useEffect(()=>{
-  const tempData = [];
-  for(const items in cartItems){
-    for(const item in cartItems[items]){
-      if(cartItems[items][item]>0){
-        tempData.push({
-          _id: items,
-          size:item,
-          quantity:cartItems[items][item]
-        })
+  useEffect(() => {
+    const tempData = [];
+    for(const items in cartItems){
+      for(const item in cartItems[items]){
+        if(cartItems[items][item] > 0){
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item]
+          })
+        }
       }
     }
-  }
-  setCartData(tempData);
-  },[cartItems])
+    setCartData(tempData);
+  }, [cartItems])
+
   return (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>
@@ -32,36 +33,56 @@ const Cart = () => {
         {
           cartData.map((item , index) => {
             const productData = products.find((product) => product._id == item._id);
-            return(
-              <div className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
-              <div className='flex items-start gap-6'>
-               <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
-               <div>
-                <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
-                <div className='flex item-center gap-5 mt-2'>
-                 <p>{currency}{productData.price}</p>
-                 <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+            
+            if (!productData) return null;
+
+            return (
+              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                     <p>{currency}{productData.price}</p>
+                     <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
+                  </div>
                 </div>
-               </div>
-              </div>
-              <input onChange={(e)=>{
-                e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id , item.size , Number(e.target.value))
-              }} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
-              <img onClick={()=>{
-                updateQuantity(item._id , item.size , 0)
-              }} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
+                
+                <input 
+                  onChange={(e) => {
+                    e.target.value === '' || e.target.value === '0' 
+                      ? null 
+                      : updateQuantity(item._id , item.size , Number(e.target.value))
+                  }} 
+                  className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' 
+                  type="number" 
+                  min={1} 
+                  defaultValue={item.quantity} 
+                />
+                
+                <img 
+                  onClick={() => updateQuantity(item._id , item.size , 0)} 
+                  className='w-4 mr-4 sm:w-5 cursor-pointer' 
+                  src={assets.bin_icon} 
+                  alt="delete item" 
+                />
               </div>
             )
           })
         }
       </div>
 
-    <div className='flex justify-end my-20'>
-    <div className='w-full sm:w-[450px]'>
-      <CartTotal/>
-    </div>
-    </div>
-
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={() => navigate('/placeorder')} className='bg-black text-white text-sm my-8 px-8 py-3 uppercase tracking-wider hover:opacity-90 active:scale-95 transition-all'>
+              Proceed to Checkout
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
