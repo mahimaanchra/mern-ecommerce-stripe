9e99fb825f5e8509dda0c7e9c42510a28c07b249
mahimaanchra <mahimaanchra@gmail.com>
2026-07-05 00:55:30 -0700

    complete login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react';
 
 const Login = () => {
   const [currentState , setCurrentState] = useState('Signup');
+  const onSubmitHandler = async (e) => {
+  e.preventDefault();
+  }
   return (
-   <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+   <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
     <div className='inline-flex items-center gap-2 mb-2 mt-10'>
      <p className='prata-regular text-3xl'>{currentState}</p>
      <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
