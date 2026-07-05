b8d5c03aa53f68fe9402e1b1a680074f9a765826
mahimaanchra <mahimaanchra@gmail.com>
2026-07-05 06:30:24 -0700

    Frontend completed

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
        <div className='flex flex-col justify-center items-start gap-6'>
         <p className='font-semibold text-xl text-gray-600'>Our Store</p>
         <p className='text-gray-500'>54709 Willims Station <br />Suite 358, Washington , USA</p>
-        <p className='text-gray-500'>Tel: (451) 555-0132 <br /> Email: admin@forever.com</p>
+        <p className='text-gray-500'>Tel: (451) 555-0132 <br />Email: admin@forever.com</p>
         <p className='font-semibold text-xl text-gray-600'>Career at Forever</p>
         <p className='text-gray-500'>Learn more about our teams and job openings.</p>
         <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
