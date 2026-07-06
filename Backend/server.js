1857c758d5f1b6a5c5eaa625675b5ba9299d08e7
mahimaanchra <mahimaanchra@gmail.com>
2026-07-06 07:29:39 -0700

    feat: Initialize Express server with CORS and basic health check endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,20 @@
-import express from "express"
+import express, { request } from "express"
 import cors from "cors"
 import 'dotenv/config'
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors())
+
+
+// API endpoints
+app.get('/' , (req , resp)=>{
+    resp.send("API Working")
+})
+
+app.listen(port , ()=> console.log('server started on Port : ' + port))
