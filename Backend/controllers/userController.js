4d4fed8121a4b8c972fdc1e7cab76e73f00d0f86
mahimaanchra <mahimaanchra@gmail.com>
2026-07-08 07:10:17 -0700

    creating new user with hashed password

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,11 +1,44 @@
-
+import userModel from "../models/userModel";
+import validator from "validator"
+import bcrypt from "bcrypt"
 //Route for user login
 const loginUser = async (req , res) => {
 
 }
 //Route for user register
 const registerUser = async (req , res) => {
+    try {
+        const {name , email , password} = req.body;
 
+        // checking user already exists or not
+
+        const exists = await userModel.findOne({email})
+        if(exists){
+            return res.json({success:false , message:"User already exists"})
+        }
+
+        //validating email format & strong passowrd
+        if(!validator.isEmail(email)){
+            return res.json({success:false , message:"Please enter a valid email"})
+        }
+        if(password.length < 8){
+            return res.json({success:false , message:"Please enter strong password"})
+        }
+
+        //hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name, 
+            email,
+            password:hashedPassword
+        })
+       const user = await newUser.save()
+
+    } catch (error) {
+        
+    }
 }
 
 //Rote for admin login
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,14 @@
-import express, { request } from "express"
+import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import dns from 'dns';
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 dns.setDefaultResultOrder('ipv4first'); 
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB();
+//connectDB();
 connectCloudinary();
 
 
@@ -17,6 +18,9 @@ app.use(cors())
 
 
 // API endpoints
+app.use('/api/user' , userRouter)
+
+
 app.get('/' , (req , resp)=>{
     resp.send("API Working")
 })
