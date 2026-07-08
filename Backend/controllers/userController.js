f16400c931f49a158a01e57ee5461e4dff226801
mahimaanchra <mahimaanchra@gmail.com>
2026-07-08 06:31:05 -0700

    user controller

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,15 @@
+
+//Route for user login
+const loginUser = async (req , res) => {
+
+}
+//Route for user register
+const registerUser = async (req , res) => {
+
+}
+
+//Rote for admin login
+const adminLogin = async (req , res) => {
+
+}
+export {loginUser , registerUser , adminLogin}
