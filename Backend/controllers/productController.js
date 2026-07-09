52deac3b19f0c4ffd4b59006a12da91aec4504fa
mahimaanchra <mahimaanchra@gmail.com>
2026-07-09 07:59:51 -0700

    product Controller

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,21 @@
+// function to add product 
+const addProduct = async (req , res) => {
+
+}
+
+//function to list product
+const listProducts = async (req , res) => {
+
+}
+
+//function to remove product
+const removeProduct = async (req , res) => {
+
+}
+
+//function for single product info
+const singleProduct = async (req , res) => {
+
+}
+
+export {addProduct , listProducts , removeProduct , singleProduct};
