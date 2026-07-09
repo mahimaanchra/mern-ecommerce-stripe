a0373d39e1a1dc23a8b472469dda12d43ebf693c
mahimaanchra <mahimaanchra@gmail.com>
2026-07-09 12:12:50 -0700

    add product

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,14 @@
 // function to add product 
 const addProduct = async (req , res) => {
-  
+  try {
+    const {name , description , price , category , subCategory , sizes , bestseller} = req.body;
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+  } catch (error) {
+    res.json({success : false , message : error.message})
+  }
 }
 
 //function to list product
