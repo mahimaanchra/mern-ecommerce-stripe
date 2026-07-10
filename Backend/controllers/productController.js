e7ac94a48fba8ee184e7b6ca2cc6010591b3dc0c
mahimaanchra <mahimaanchra@gmail.com>
2026-07-10 07:13:31 -0700

    image url from clodinary

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,3 +1,4 @@
+import {v2 as cloudinary} from "cloudinary";
 // function to add product 
 const addProduct = async (req , res) => {
   try {
@@ -6,6 +7,16 @@ const addProduct = async (req , res) => {
     const image2 = req.files.image2 && req.files.image2[0];
     const image3 = req.files.image3 && req.files.image3[0];
     const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1 , image2 , image3 , image4].filter((item)=>item !== undefined);
+
+    let imagesUrl = await  Promise.all(
+      images.map(async (item)=>{
+        let result = await cloudinary.uploader.upload(item.path , {resource_type:'image'});
+        return result.secure_url;
+      })
+    )
+
   } catch (error) {
     res.json({success : false , message : error.message})
   }
