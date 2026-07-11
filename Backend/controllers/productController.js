130f74893be57b251092ace57016a278e964b640
mahimaanchra <mahimaanchra@gmail.com>
2026-07-11 11:24:54 -0700

    single product

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -62,7 +62,13 @@ const removeProduct = async (req , res) => {
 
 //function for single product info
 const singleProduct = async (req , res) => {
-
+   try {
+    const {productId} = req.body;
+    const product = await productModel.findById(productId);
+    res.json({success:true , product})
+   } catch (error) {
+    res.json({success : false , message : error.message});
+   }
 }
 
 export {addProduct , listProducts , removeProduct , singleProduct};
