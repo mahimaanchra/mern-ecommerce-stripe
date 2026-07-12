0452fedaaf20cdfa8023ce77152885685ad4f93d
mahimaanchra <mahimaanchra@gmail.com>
2026-07-12 07:58:09 -0700

    admin login

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -74,6 +74,17 @@ const registerUser = async (req , res) => {
 
 //Rote for admin login
 const adminLogin = async (req , res) => {
-
+  try {
+     const {email , password} = req.body;
+   if(email === process.eventNames.ADMIN_EMAIL && process.env.ADMIN_PASSWORD){
+    const token = jwt.sign(email+password , process.env.JWT_SECRET)
+    res.json({success: true , token})
+   }else{
+    res.json({success:false , message:"Invalid credentials"})
+   }
+  } catch (error) {
+    console.log(error);
+        res.json({success:false , message:error.message})
+  }
 }
 export {loginUser , registerUser , adminLogin};
