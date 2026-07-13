3f17f86cd7ada28903a7af55b9a9b58ab5a81989
mahimaanchra <mahimaanchra@gmail.com>
2026-07-13 09:57:04 -0700

    login page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login';
 
 function App() {
+  const [token , setToken] = useState('');
   return (
-   <> 
+   
    <div className='bg-gray-50 min-h-screen'>
+    {token === '' ? <Login/>
+   :
+   <>
    <Navbar/>
    <hr />
    <div className='flex w-full'>
     <Sidebar/>
-   </div>
+    <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+     <Routes>
+      <Route path='/add' element={<Add/>}/>
+      <Route path='/list' element={<List/>}/>
+      <Route path='/orders' element={<Orders/>}/>
+     </Routes>
+    </div>
    </div>
    </>
+}
+   </div>
+   
   )
 }
 
