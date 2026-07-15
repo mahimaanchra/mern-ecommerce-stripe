1016c2de4bee67ada0b4bca2af7ed8101fea5606
mahimaanchra <mahimaanchra@gmail.com>
2026-07-15 16:25:56 -0700

    cartController

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// add to cart
+const addToCart = async (req , res) => {
+    
+}
+// update cart
+const updateCart = async (req , res) => {
+
+}
+// get cart
+const getUserCart = async (req , res) => {
+
+}
+
+export {addToCart , updateCart , getUserCart};
