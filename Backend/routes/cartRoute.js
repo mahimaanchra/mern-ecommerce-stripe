c890d7b7a885b98c1c7ba4970145be68cb05764d
mahimaanchra <mahimaanchra@gmail.com>
2026-07-15 17:04:34 -0700

    auth middleware

--- Backend/routes/cartRoute.js
+++ Backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { addToCart , getUserCart , updateCart } from '../controllers/cartController'
+import authUser from '../middleware/auth'
 
 const cartRouter = express.Router()
 
-cartRouter.post('/get' , getUserCart)
-cartRouter.post('/update' ,updateCart)
-cartRouter.post('/add', addToCart)
+cartRouter.post('/get' , authUser , getUserCart)
+cartRouter.post('/update' ,authUser,updateCart)
+cartRouter.post('/add',authUser, addToCart)
 
 export default cartRouter
