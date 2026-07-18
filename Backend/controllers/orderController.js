3e2dcb50b237b0f26b63f3784825fc828d5ce7b6
mahimaanchra <mahimaanchra@gmail.com>
2026-07-18 20:30:30 +0530

    order controller

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,36 @@
+//placing orders using cod methods
+
+const placeOrder = async (req , res) => {
+
+}
+
+//placing order using Stripe method
+
+const placeOrderStripe = async (req , res) => {
+
+}
+
+//placing order using Razorpay method
+
+const placeOrderRazorpay = async (req , res) => {
+
+}
+
+//All orders data for admin panel
+
+const allOrders = async(req , res) => {
+    
+}
+
+//All orders data for frontend
+
+const userOrders = async(req , res) => {
+
+}
+
+//update order status from admin panel
+ const updateStatus = async(req , res) => {
+
+ }
+
+ export {placeOrder , placeOrderRazorpay , placeOrderStripe , allOrders , userOrders , updateStatus}
