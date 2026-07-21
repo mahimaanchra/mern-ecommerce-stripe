78193a4ee48cbf4bb06468b873cd2ec8cf585bf8
mahimaanchra <mahimaanchra@gmail.com>
2026-07-21 22:53:41 +0530

    showing orders

--- Frontend/src/pages/Orders.jsx
+++ Frontend/src/pages/Orders.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 
 const Orders = () => {
-  const {products , currency} = useContext(ShopContext);
+  const {backendUrl, token , currency} = useContext(ShopContext);
+  const [orderData , setOrderdata] = useState({});
+  const loadOrderData = async () => {
+    try {
+      if(!token){
+        return null;
+      }
+      const response = await axios.post(backendUrl + '/api/order/userorders' , {} , {headers:{token}});
+      if(response.data.success){
+        let allOrderItem = [];
+        response.data.orders.map((order)=>{
+          order.items.map((item)=>{
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
+            allOrderItem.push(item)
+          })
+        })
+        setOrderdata(allOrderItem.reverse());
+      }
+    } catch (error) {
+      
+    }
+  }
+  useEffect(()=>{
+loadOrderData();
+  },token)
   return (
     <div className='border-t pt-16'>
       <div className='text-2xl'>
        <Title text1={'MY'} text2={'ORDERS'}/>
       </div>
       <div>
-        {products.slice(1,4).map((item , index)=>(
+        {orderData.map((item , index)=>(
           <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md;justify-between gap-4'>
            <div className='flex items-start gap-6 text-sm'>
             <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
