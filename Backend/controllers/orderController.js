f6ae11d556faf68586545c217fb75f3d6cc3da48
mahimaanchra <mahimaanchra@gmail.com>
2026-07-23 18:57:27 +0530

    stripe method

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,5 +1,14 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js";
+//global variables
+const currency = "usd";
+const deliveryCharge = 10;
+
+
+//gateway initialized
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 //placing orders using cod methods
 
 const placeOrder = async (req , res) => {
@@ -26,7 +35,51 @@ const placeOrder = async (req , res) => {
 //placing order using Stripe method
 
 const placeOrderStripe = async (req , res) => {
+   try {
+    const {userId , items , amount , address } = req.body;
+    const {origin} = req.headers;
+     const orderData = {
+     userId ,
+     items , 
+     amount , 
+     address ,
+     paymentMethod:"Stripe",
+     payment:false,
+     date:Date.now()
+    }
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
 
+    const line_items = items.map((item)=>({
+        price_data:{
+            currency:currency,
+            product_data: {
+                name:item.name
+            },
+            unit_amount : item.price*100
+        },
+         quantity : item.quantity
+    }))
+    line_items.push(
+       { price_data:{
+            currency:currency,
+            product_data: {
+                name:"Delivery charges"
+            },
+            unit_amount: deliveryCharge*100
+        },
+         quantity : 1
+    })
+    const session = await stripe.checkout.sessions.create({
+        success_url: `${origin}/verify?success=true&iorderId=${newOrder._id}`,
+        cancel_url: `${origin}/verify?success=false&iorderId=${newOrder._id}`,
+        line_items,
+        mode:'payment',
+    })
+    res.json({success:true,session_url:session.url})
+   } catch (error) {
+     response.json({success:false,message:error.message});
+   }
 }
 
 //placing order using Razorpay method
